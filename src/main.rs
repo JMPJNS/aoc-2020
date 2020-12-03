5031f2d39a5aef200ed1234f7c084daf13eb51b8
Jonas <jonas@jmp.blue>
2020-12-03 09:03:17 +0100

    day3

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod day1;
 mod day2;
+mod day3;
 
 fn main() {
     println!("Day 1, Problem 1");
@@ -12,5 +13,11 @@ fn main() {
     day2::uno();
     println!("Day 2, Problem 2");
     day2::due();
+    println!();
+
+    println!("Day 3, Problem 1");
+    day3::uno();
+    println!("Day 3, Problem 2");
+    day3::due();
     println!();
 }
